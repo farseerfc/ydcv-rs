085308e17049ba001c3c880db53c6d2fb5b80db5
Jiachen Yang <jc-yang@ist.osaka-u.ac.jp>
2015-06-29 20:27:17 +0900

    add selection support

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ extern crate getopts;
 extern crate linenoise;
 
 use std::env;
+use std::process::Command;
+use std::thread;
 use getopts::Options;
 use hyper::Client;
 
@@ -18,12 +20,27 @@ mod ydclient;
 
 use ydclient::YdClient;
 
-#[allow(dead_code)]
+
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [options] words", program);
     print!("{}", opts.usage(&brief));
 }
 
+fn lookup_explain(client: &mut Client, word: &String){
+        match client.lookup_word(&word){
+        Ok(ref result) =>  println!("{}", result.explain()),
+        Err(err) => println!("Error during lookup word {}: {:?}", word, err)
+    }
+}
+
+fn get_clipboard() -> String {
+    if let Ok(out) = Command::new("xsel").arg("-o").output() {
+        if let Ok(result) = String::from_utf8(out.stdout) {
+            return result;
+        }
+    }
+    return "".to_string();
+}
 
 #[allow(dead_code)]
 fn main() {
@@ -32,6 +49,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
+    opts.optflag("x", "selection", "show explaination of current selection");
 
     let matches = match opts.parse(&args[1..]){
         Ok(m) => m,
@@ -47,18 +65,30 @@ fn main() {
 
     if matches.free.len() > 0 {
         for word in matches.free {
-            match client.lookup_word(&word){
-                Ok(ref result) =>  println!("{}", result.explain()),
-                Err(err) => println!("Error during lookup word {}: {:?}", word, err)
-            }
+            lookup_explain(&mut client, &word);
         }
     } else {
-        while let Some(word) =  linenoise::input("> ") {
-            match client.lookup_word(&word){
-                Ok(ref result) =>  println!("{}", result.explain()),
-                Err(err) => println!("Error during lookup word {}: {:?}", word, err)
+        if matches.opt_present("x") {
+            let mut last = get_clipboard();
+            print!("Waiting for selection> ");
+            loop {
+                thread::sleep_ms(100);
+                let curr = get_clipboard();
+
+                if curr != last {
+                    last = curr.clone();
+                    if last.len() > 0 {
+                        lookup_explain(&mut client, &curr);
+                        println!("Waiting for selection> ");
+                    }
+                }
+
             }
-            linenoise::history_add(&word);
+        } else {
+            while let Some(word) =  linenoise::input("> ") {
+                lookup_explain(&mut client, &word);
+                linenoise::history_add(&word);
+            }
         }
     }
     return;
